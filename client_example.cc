2a433673257a8819fddda25f03cbac1fd0669a86
weian.cheng <weian.cheng@xrspace.io>
2020-08-27 14:31:17 +0800

    fix sample codes

--- client_example.cc
+++ client_example.cc
@@ -1,11 +1,51 @@
 #include "TCPClient.h"
+
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string>
+#include <cstring>
+
+#define BUFFER_SIZE 4096
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 6666
+
+using namespace std;
+
 
 int main(int argc, char **argv) {
     TCPClient client;
+    FILE *filp = nullptr;
+    char buffer[BUFFER_SIZE];
 
-    client.start("127.0.0.1", 6666);
+    filp = fopen("output.jpg", "wb");
+    if (filp == nullptr) {
+        printf("Error, failed to create output file...\n");
+        return -1;
+    }
+
+    if (!client.start(SERVER_IP, SERVER_PORT)) {
+        printf("Error, failed to connect to server(%s:%d)\n", SERVER_IP, SERVER_PORT);
+        fclose(filp);
+        return -1;
+    }
+
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        int length = client.recvFromServer(buffer, BUFFER_SIZE);
+        if (length == 0) {
+            break;
+        }
+
+        printf("length: %d\n", length);
+
+        fwrite(buffer, sizeof(char), length, filp);
+    }
+
     client.stop();
+
+    fclose(filp);
 
     return 0;
 }
--- server_example.cc
+++ server_example.cc
@@ -1,10 +1,46 @@
 #include "TCPServer.h"
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string>
+#include <cstring>
+
+#define SERVER_PORT 6666
+#define BUFFER_SIZE 4096
+
+using namespace std;
+
 
 int main(int argc, char **argv) {
     TCPServer server;
-    server.start(6666);
-    server.waitForClient();
+    FILE *filp = nullptr;
+    char buffer[BUFFER_SIZE];
+    string eof("");
+
+    filp = fopen("test.jpg", "r");
+    if (filp == nullptr) {
+        printf("Error, failed to open file...\n");
+        return -1;
+    }
+
+    server.start(SERVER_PORT);
+
+    int clientfd = server.waitForClient();
+
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        int length = fread(buffer, sizeof(char), BUFFER_SIZE, filp);
+        if (length == 0) {
+            server.sendToClient(clientfd, eof.c_str(), eof.size());
+            break;
+        }
+
+        server.sendToClient(clientfd, buffer, length);
+    }
+
     server.stop();
+    fclose(filp);
+
     return 0;
 }
